47a7a6770d7b6ed49fd3920d397c8a6e9d3a1731
Jacques <dorvalfabrice8@gmail.com>
2020-04-02 13:10:03 -0700

    La reste du Modulo est <remplace ici>

--- b000000000-structure.ps1
+++ b000000000-structure.ps1
@@ -1,14 +1,20 @@
-<#
-.SYNOPSIS
-    Ce script est une laboratoire Powershell
+[String]$personneNom = "Alice"
+[Int]$personneAge = 35
 
-.DESCRIPTION
-    Ce script est utilisé pour le laboratoire de programmation en Powershell.
+# Test sur l'age de la personne
+switch ($personneAge) {
+    {$_ -le 25} {Write-Host 'en pleine jeunesse'; break }
+    {$_ -le 35} {Write-Host 'en pleine force vive'; break }
+    {$_ -le 45} {Write-Host 'en pleine maturité'; break }
+    Default {Write-Host 'en pleine serénité'}
+}
 
-.NOTES
-    Author: CollegeBoreal
-    Derniere mise à jour: yyyy-mm-dd
 
-#>
+$villes = "Toronto","Mississauga","Scarborough","Brampton"
+for ($i = 0; $i -le $villes.Count; $i = $i + 1) {
+    $villes[$i]
+}
 
-Write-Host "Bonjour Monde"
+Clear-Host
+
+"{0}" -F ($villes.Count % 2)
